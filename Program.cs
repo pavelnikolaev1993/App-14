f9905d53d7ace2a9aa7e44f9f03b1623eb48d5a6
pavel <pavel@DESKTOP-AI7TEAC>
2022-03-02 08:26:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿Cat marta = new Cat("Кошка");
+marta.Showinfo();
+Dog reks = new Dog("Собака");
+reks.Showinfo();
+
+
+
+
+abstract class Animal
+{ 
+public abstract string Name { get; set;  }
+
+    public Animal(string name)
+    { 
+    Name = name;
+    }
+    public abstract void Say();
+   
+    public void Showinfo()
+    {
+        
+        Console.WriteLine(Name);
+        Say();
+
+    }
+   
+}
+class Cat : Animal
+{
+    private string say;
+    private string name;
+    public override string Name
+    {
+        get => name;
+        set => name = value;
+    }
+    public Cat(string name)
+        :base(name)
+    {
+       
+    }
+    public override void Say()
+    {
+        Console.WriteLine("Мяу");
+    }
+
+
+
+}
+class Dog : Animal
+{
+    private string say;
+    private string name;
+    public override string Name
+    {
+        get => name;
+        set => name = value;
+    }
+    public Dog(string name)
+        : base(name)
+    {
+        
+    }
+    public override void Say()
+    {
+        Console.WriteLine("Гав");
+    }
+
+
+
+}
+
